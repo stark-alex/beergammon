f37cb2e3e906002216504a8321377b369006e496
Alex Stark <alex.stark@cerner.com>
2020-04-22 21:18:44 -0500

    adding drink graph to endgame dialog

--- src/BeergammonBoard/congratulations.js
+++ src/BeergammonBoard/congratulations.js
@@ -7,7 +7,9 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
+import LineChart from 'react-linechart';
 
+import colors from '../colors';
 import PlayersNamesContext from "./playersNamesContext";
 
 class Congratulations extends Component {
@@ -33,11 +35,51 @@ class Congratulations extends Component {
     window.open(`${process.env.PUBLIC_URL}/`, "_self");
   };
 
+  generateGraphData = (playerNames) => {
+   // Add up all the drinks per turn per player.
+   let drinksPerTurn = [ [], [] ];
+   this.props.G.drinks.forEach(drink => {
+      if (!drinksPerTurn[drink.player][drink.turn]) {
+         drinksPerTurn[drink.player][drink.turn] = 0;
+      }
+      drinksPerTurn[drink.player][drink.turn] += drink.count;
+   });
+
+   // Build up the graph points based on drinks per turn.
+   let accumulatedDrinks = [ [ {x: 0, y: 0} ], [ {x: 0, y: 0} ] ]
+   for (let turn = 1; turn < this.props.ctx.turn; turn++) {
+      for (let player = 0; player <= 1; player++) {
+         let drinksThisTurn = drinksPerTurn[player][turn] ? drinksPerTurn[player][turn] : 0;
+         accumulatedDrinks[player][turn] = {
+            x: turn, 
+            y: accumulatedDrinks[player][turn-1].y + drinksThisTurn,
+         }
+      }
+   }
+
+   // combine data with chart info
+   return [
+      {
+         name: playerNames[0],
+         color: colors["0"],
+         points: accumulatedDrinks[0],
+      },
+      {
+         name: playerNames[1],
+         color: colors["1"],
+         points: accumulatedDrinks[1],
+      },
+   ];
+}
+
   render() {
       return (
          <PlayersNamesContext.Consumer>
             {playersNames => {
                const playerName = this.props.ctx.gameover && playersNames[this.props.ctx.gameover.winner];
+
+               const data = this.generateGraphData(playersNames);
+
                return (
                   <Dialog open={this.state.open} fullWidth onClose={this.handleClose}>
                      {this.props.ctx.gameover && (
@@ -47,6 +89,14 @@ class Congratulations extends Component {
                         <DialogContent>
                            <Grid container justify="center">
                               <CardContent>{playerName} Wins!</CardContent>
+                              <LineChart 
+                                 width={600}
+                                 height={400}
+                                 xLabel="Turns"
+                                 yLabel="Drinks"
+                                 showLegends
+                                 data={data}
+                              />
                            </Grid>
                         </DialogContent>
                      )}
