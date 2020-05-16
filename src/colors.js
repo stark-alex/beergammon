694f004fcd2d85c66d27d27c597058b605e2e341
Alex Stark <alex.stark@cerner.com>
2020-05-15 21:34:06 -0500

    moving to 0.1.14, adding in hand marker, cleaning up colors for khud

--- src/colors.js
+++ src/colors.js
@@ -1,19 +1,11 @@
 const colors = {
-   "0": "#3D5A80", 
-   "1": "#F26430", 
-   "boardDark": "#4C4C47", 
-   "boardLight": "#E0E2DB",
-   "background": "#203854" 
+   "0": "#646F4B",
+   "1": "#8B2635",
+   "boardDark": "#A69888",
+   "boardLight": "#EEE0CB",
+   "background": "#202C39",
+   "pokey_home": "#392A16",
+   "in_hand": "#F9C22E",
 }
-
-/* Attempt at red/gree pieces...
-const colors = {
-   "0": "#65743A", //green
-   "1": "#CC2936", // red
-   "boardDark": "#49667D", // light blue
-   "boardLight": "#F9EAC4", // cream
-   "background": "#203854" // dark
-}
-*/
 
 export default colors
